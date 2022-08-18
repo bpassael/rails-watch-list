e736b37c1fe3f478f92e9505bfa6f4efdbe11e56
Bastien Perez Assael <perez.bas@gmail.com>
2022-08-18 22:56:31 +0200

    watchlist

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:new, :create, :index, :show] do
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new', as: :new_list
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list do
+    resources :bookmarks, only: [:new, :create, :destroy]
+    # get 'list/:id/bookmarks/new', to: 'bookmarks#new'
+    # post 'list/:id/bookmarks', to: 'bookmarks#create'
+  end
+
 end
